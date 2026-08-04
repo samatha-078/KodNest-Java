1019af3f8186f7cb727534b42fa012c7ab61149e
samatha-078 <samathasam708@gmail.com>
2026-08-04 12:04:19 +0530

    trial 1

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/practice.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/practice.java
@@ -1,14 +1,16 @@
+
 public class practice {
+
     public static void main(String[] args) {
-       //Write your
-       //  code here
-    //    System.out.println("samuuu");
-    System.out.println("Monday");
-    System.out.print("Tuesday");
-    System.out.println("Wednesday");
-    System.out.println("Thursday");
-    System.out.print("Friday");
-    System.out.println("Saturday");
-    System.out.println("Sunday");
+        //Write your
+        //  code here
+        //    System.out.println("samuuu");
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("Wednesday");
+        System.out.println("Thursday");
+        System.out.print("Friday");
+        System.out.println("Saturday");
+        System.out.println("Sunday");
     }
 }
