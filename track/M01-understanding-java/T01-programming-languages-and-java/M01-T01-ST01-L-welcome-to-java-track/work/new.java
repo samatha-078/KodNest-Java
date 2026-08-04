6bdd82efd8961a558586b586f7d80b836bbc1b33
samatha-078 <samathasam708@gmail.com>
2026-08-04 12:16:44 +0530

    trial 2

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/new.java
@@ -0,0 +1,15 @@
+public class new{
+
+    public static void main(String[] args) {
+        //Write your
+        //  code here
+        //    System.out.println("samuuu");
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("Wednesday");
+        System.out.println("Thursday");
+        System.out.print("Friday");
+        System.out.println("Saturday");
+        System.out.println("Sunday");
+    }
+}
