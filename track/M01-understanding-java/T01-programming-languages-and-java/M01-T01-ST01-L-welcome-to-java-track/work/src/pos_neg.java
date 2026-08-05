cfdfd56fbf8603c8a3fb3f853678edf86a6cde78
samatha <samathasam708@gmail.com>
2026-08-05 11:04:06 +0530

    day2-j1

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/pos_neg.java
@@ -0,0 +1,16 @@
+public class pos_neg
+{
+    public static void main(String[] args) {
+        int num=7;
+        if(num>0){
+            System.out.println("positive");
+
+        }
+        else if(num<0){
+            System.out.println("Negative");
+        }
+        else {
+            System.out.println("zero");
+        }
+    }
+}
