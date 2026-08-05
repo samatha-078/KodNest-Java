58ef83c5edebb886b51b17ba436d845fc1249ea1
samatha <samathasam708@gmail.com>
2026-08-05 11:36:08 +0530

    day2-j2

---
+++ track/M02/prgm1.java
@@ -0,0 +1,9 @@
+public class prgm1{
+    public static void main(String[] args) {
+        
+        int a=10;
+        int b=20;
+        String res = a<b ? "a is less than b" : "b is greater than a";
+        System.out.println(res);
+    }
+}
