1be3991b84c5f3d57f472f5a00a888800b54f4e7
samatha <samathasam708@gmail.com>
2026-08-05 23:21:30 +0530

    changes

---
+++ track/M02/prgm5.java
@@ -0,0 +1,11 @@
+public class prgm5{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+        int res =  a>b && a>c ? a : b>c ? b : c ;
+        System.out.print(res);
+
+        
+    }
+}
