6849dd26232e3b2245c368fc1c73ed5cfcc4f7da
samatha <samathasam708@gmail.com>
2026-08-06 19:51:11 +0530

    day 3 changes

---
+++ track/M02/prgm10.java
@@ -0,0 +1,12 @@
+public class prgm10 {
+    public static void main(String[] args) {
+        for(int i=1;i<=3;i++){
+            for(int j=1;j<=4;j++){
+                if(j==2){
+                    continue;
+                }
+                System.out.println("i: " +i +"j: "+j);
+            }
+        }
+    }
+}
